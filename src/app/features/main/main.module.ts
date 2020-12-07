dcb8f1c4da41d0632857182e985469d795d161b1
uli-hermanns <uli.hermanns@moser.de>
2020-12-07 08:32:44 +0100

    3 files modified

--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from "@angular/common";
 import { MainComponent } from "./main.component";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
 import { TitlebarComponent } from "./titlebar/titlebar.component";
 import { SharedModule } from "../../shared/shared.module";
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatGridListModule, SharedModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatSidenavModule,
+    SharedModule
+  ],
   declarations: [MainComponent, TitlebarComponent],
   exports: [MainComponent]
 })
