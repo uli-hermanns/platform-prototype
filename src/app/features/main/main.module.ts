229000afb6181b01b56651538946f41921a0f008
uli-hermanns <uli.hermanns@moser.de>
2020-12-07 07:11:53 +0100

    Tiles eingeführt.

--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MainComponent } from "./main.component";
+import { MatGridListModule } from "@angular/material/grid-list";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { TitlebarComponent } from './titlebar/titlebar.component';
+import { TitlebarComponent } from "./titlebar/titlebar.component";
+import { SharedModule } from "../../shared/shared.module";
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatGridListModule, SharedModule],
   declarations: [MainComponent, TitlebarComponent],
   exports: [MainComponent]
 })
