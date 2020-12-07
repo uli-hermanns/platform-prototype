b4b76aa4437783f14e1b6c9e3e09111ea025619b
uli-hermanns <uli.hermanns@moser.de>
2020-12-07 08:55:50 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
@@ -11,6 +12,7 @@ import { CoreModule } from "./core/core/core.module";
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     CoreModule,
     SharedModule,
