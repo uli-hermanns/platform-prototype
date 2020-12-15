dab811fbc4776865517b91878950f2f616c68f61
uli-hermanns <uli.hermanns@moser.de>
2020-12-15 09:40:46 +0100

    2 files modified

--- src/app/application.component.ts
+++ src/app/application.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "application",
-  templateUrl: "./allround.component.html",
-  styleUrls: ["./allround.component.css"]
+  selector: "pla-application",
+  templateUrl: "./application.component.html",
+  styleUrls: ["./application.component.css"]
 })
-export class AllroundComponent {}
+export class ApplicationComponent {}
