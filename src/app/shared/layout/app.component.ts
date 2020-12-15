4fc7ffd4921318895c804d426752c3a1a52304dd
uli-hermanns <uli.hermanns@moser.de>
2020-12-15 12:54:19 +0100

    2 files modified

--- src/app/shared/layout/app.component.ts
+++ src/app/shared/layout/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "pla-app",
+  selector: "flex-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
