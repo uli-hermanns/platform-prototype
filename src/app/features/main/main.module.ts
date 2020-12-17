da9c4ae5eaa33ecfd107c0515574187c77392d8c
uli-hermanns <uli.hermanns@moser.de>
2020-12-17 12:40:15 +0100

    1 file modified

--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { MainComponent } from "./main.component";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { SharedModule } from "../../shared/shared.module";
+import { MatTabsModule } from "@angular/material/tabs";
 
 @NgModule({
-  imports: [CommonModule, MatGridListModule, SharedModule],
+  imports: [CommonModule, MatGridListModule, SharedModule, MatTabsModule],
   declarations: [MainComponent],
   exports: [MainComponent]
 })
