c457f31cd3963e1ae517957ee6825a07594de91b
uli-hermanns <uli.hermanns@moser.de>
2020-12-17 11:53:04 +0100

    1 file modified

--- src/app/shared/layout/app/app-details/app-details.component.ts
+++ src/app/shared/layout/app/app-details/app-details.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-app-details',
-  templateUrl: './app-details.component.html',
-  styleUrls: ['./app-details.component.css']
+  selector: "flex-app-details",
+  templateUrl: "./app-details.component.html",
+  styleUrls: ["./app-details.component.css"]
 })
 export class AppDetailsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
