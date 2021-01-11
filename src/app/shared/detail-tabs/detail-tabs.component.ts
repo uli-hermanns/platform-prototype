1d6319ee38f6d2b6f2f24f633f6a10349f424764
uli-hermanns <uli.hermanns@moser.de>
2021-01-11 10:06:41 +0100

    2 files modified

--- src/app/shared/detail-tabs/detail-tabs.component.ts
+++ src/app/shared/detail-tabs/detail-tabs.component.ts
@@ -12,7 +12,7 @@ import { DetailTabComponent } from "./detail-tab/detail-tab.component";
 @Component({
   selector: "flex-detail-tabs",
   templateUrl: "./detail-tabs.component.html",
-  styleUrls: ["./detail-tabs.component.css"],
+  styleUrls: ["./detail-tabs.component.css"]
 })
 export class DetailTabsComponent implements AfterViewInit {
   @ViewChild(MatTabGroup)
@@ -23,14 +23,20 @@ export class DetailTabsComponent implements AfterViewInit {
 
   constructor() {}
 
-  public ngAfterViewInit() {
-  }
+  public ngAfterViewInit() {}
 
   public get visibleTabs(): DetailTabComponent[] {
-      return this.tabs.filter(tab => tab.visible)
+    return this.tabs.filter(tab => tab.visible);
   }
 
   public get hiddenTabs(): DetailTabComponent[] {
-      return this.tabs.filter(tab => !tab.visible)
+    return this.tabs.filter(tab => !tab.visible);
+  }
+
+  public show(tab: DetailTabComponent) {
+    const tabs = this.visibleTabs;
+    tabs[tabs.length - 1].visible = false;
+    tab.visible = true;
+    this.tabGroup.selectedIndex = tabs.length - 1;
   }
 }
