1993908ad524adae8b14ffbcc996229808bff0dc
uli-hermanns <uli.hermanns@moser.de>
2021-01-21 12:05:02 +0100

    1 file modified

--- src/app/shared/forms/form-autosave.directive.ts
+++ src/app/shared/forms/form-autosave.directive.ts
@@ -9,7 +9,6 @@ import {
 } from "@angular/core";
 import { AbstractControl, FormGroup, FormGroupDirective } from "@angular/forms";
 import { DomHelper } from "../../core/data/dom-helper";
-// import * as log4js from "log4js";
 
 export type AutosaveEventArgs = { value: any }
 
@@ -17,6 +16,8 @@ export type AutosaveEventArgs = { value: any }
   selector: "[flexFormAutosave]"
 })
 export class FormAutosaveDirective implements OnInit {
+
+  private logger: Console;
 
   @Input("flexFormAutosave")
   formData: Object | undefined;
@@ -28,9 +29,8 @@ export class FormAutosaveDirective implements OnInit {
     private formGroupDirective: FormGroupDirective,
     private el: ElementRef<HTMLFormElement>
   ) {
-    // this.logger = log4js.getLogger();
-    // this.logger.level = "debug"; // default level is OFF - which means no logs at all.
-    console.debug("Form Autosave Directive initialized.");
+    this.logger = console;
+    this.logger.debug("Form Autosave Directive initialized.");
   }
 
   public ngOnInit() {
@@ -39,11 +39,11 @@ export class FormAutosaveDirective implements OnInit {
     this.form.valueChanges.subscribe(data => {
       if (this.nativeElement.hasPointerCapture(1)) {
         this.nativeElement.releasePointerCapture(1);
-        console.debug("Form Autosave capture released.");
+        this.logger.debug("Form Autosave capture released.");
       }
 
       if (this.form.dirty && !this.form.invalid) {
-        console.debug("Form Autosave saving.");
+        this.logger.debug("Form Autosave saving.");
         this.formSave.emit({ value: data });
         this.form.markAsPristine();
       }
@@ -64,7 +64,7 @@ export class FormAutosaveDirective implements OnInit {
       setTimeout(() => {
         if (this.form.invalid) {
           this.nativeElement.setPointerCapture(1);
-          console.debug("Form Autosave capture set.");
+          this.logger.debug("Form Autosave capture set.");
 
           const element: HTMLElement | null = this.nativeElement.querySelector(
             "input.ng-invalid, select.ng-invalid"
