b3212f21ce134ddcac157f7123c94d9d9ce73943
uli-hermanns <uli.hermanns@moser.de>
2021-01-22 10:11:29 +0100

    1 file modified

--- src/app/shared/forms/form-autosave-builder.service.ts
+++ src/app/shared/forms/form-autosave-builder.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
+/**
+ *  Allows to build Reactive Forms using the autosave feature.
+ */
 @Injectable()
 export class FormAutosaveBuilder {
   constructor(private fb: FormBuilder) {}
 
+  /**
+   *  Creates a form group.
+   *
+   *  @param controls The controls.
+   *  @returns the form group.
+   */
   public group(controls: { [key: string]: FormControl }): FormGroup {
     return this.fb.group(controls, { updateOn: "blur" });
   }
